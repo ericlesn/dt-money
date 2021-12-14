46da0c440e86fd11c4d26c39ffe542bb96278fb5
ericles <ericlesnunes1@outlook.com>
2021-12-13 23:08:06 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createServer, Model, Server} from 'miragejs'
+import {App} from './App';
+
+
+createServer({
+  models:{
+    transaction: Model,
+  },
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'qualquer coisa',
+          type: 'deposit',
+          category: 'random',
+          amount: 6000,
+          createdAt: new Date('2021-12-10 21:34:00')
+        }
+      ]
+    })
+
+  },
+
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', ()=>{
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request)=>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
